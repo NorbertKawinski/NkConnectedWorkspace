a492ca74f9bb72376659e10f3c4e88ff543e1855
Norbert Kawiński <norbert@kawinski.net>
2021-08-13 19:58:52 +0200

    Fiddling with networking and input hooks

--- src/main/java/net/kawinski/connectedworkspace/nettyexamples/TimeDecoder.java
+++ src/main/java/net/kawinski/connectedworkspace/nettyexamples/TimeDecoder.java
@@ -3,16 +3,13 @@ package net.kawinski.connectedworkspace.nettyexamples;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.ReplayingDecoder;
 
 import java.util.List;
 
-public class TimeDecoder extends ByteToMessageDecoder { // (1)
+public class TimeDecoder extends ReplayingDecoder<Void> {
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
-        if (in.readableBytes() < 4) {
-            return;
-        }
-
         out.add(new UnixTime(in.readUnsignedInt()));
     }
 }
